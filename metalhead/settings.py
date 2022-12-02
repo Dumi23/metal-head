ddf38fed0076afa5889b610dc3e1edb1c228a833
Dumi23 <malidamjan@outlook.com>
2022-12-02 18:36:52 +0100

    change database provider

--- metalhead/settings.py
+++ metalhead/settings.py
@@ -79,12 +79,8 @@ WSGI_APPLICATION = 'metalhead.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'postgres',
-        'USER': 'postgres',
-        'HOST': 'localhost',
-        'PORT': 5432,
-        'PASSWORD':'postgres'
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
